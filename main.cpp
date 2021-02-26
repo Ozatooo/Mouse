d9b8bb5b328672760cd06834522c581bbc8a3dc6
Ozatooo <61974211+Ozatooo@users.noreply.github.com>
2021-02-26 10:52:21 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+	ofstream inFile;
+class file
+{
+
+public:
+	int loop(float x, float y);
+
+};
+int file::loop(float x, float y)
+{
+    inFile<<x<<","<<y<<endl;   
+}
+
+
+
+
+
+using namespace std;
+int main()
+{
+	file f;
+	cout << "Hello World!\n";
+	inFile.open("wynik.txt");
+
+	while (1)
+
+	{
+		POINT cursorPos;
+		GetCursorPos(&cursorPos);
+		float x = 0;
+		x = cursorPos.x;
+		float y = 0;
+		y = cursorPos.y;
+		int popx;
+		int popy;
+		if ((GetKeyState(VK_LBUTTON) & 0x8000) != 0)
+		{
+
+			if(popx!=x || popy!=y){
+			
+			cout << x << ',' << y <<endl;
+			f.loop(x,y);
+			
+			}
+			popx=x;
+			popy=y;
+			
+
+		}
+		
+
+	}
+	
+	    inFile.close();   
+
+
+}
